06e728ca99f3fd2841b4f43f4f22a6d7beeba9ce
Asa Salcido <asasalcido14@email.arizona.edu>
2020-10-28 15:04:26 -0700

    addjs

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function setQuery(evt) {
 }
 
 function getResults(query) {
-    fetch(`${api.base}weather?q=${query}&sunits=metric&APPID=${api.key}`)
+    fetch(`${api.baseurl}weather?q=${query}&sunits=metric&APPID=${api.key}`)
     .then(weather => {
         return weather.json();
     }).then(displayResults);
@@ -30,13 +30,13 @@ function displayResults (weather){ 
     date.innerText = dateBuilder(now);
 
     let temp = document.querySelector('.current .temp');
-    temp.innerTExt = `${Math.round(weather.main.temp)}<span>°c </span>`;
+    temp.innerTExt = `${Math.round(weather.main.temp).toFixed(0)}<span>°c </span>`;
 
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
     let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${weather.main.temp_min}°c / ${weather.main.temp_max}°c`
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`
 };
 
 function dateBuilder (d) {
