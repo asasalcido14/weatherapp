47c3f00d52d2ef5226f9d326be2e644870275947
Asa Salcido <asasalcido14@email.arizona.edu>
2020-10-27 20:39:30 -0700

    add date builder

--- main.js
+++ main.js
@@ -32,5 +32,13 @@ function displayResults (weather){
 }
 
 function dateBuilder (d) {
-    let months = ["Januar"]
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August",
+"September", "October", "November", "December"];
+    let days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullyear();
+    
+    return `${day} ${date} ${month} ${year}`;
 }
